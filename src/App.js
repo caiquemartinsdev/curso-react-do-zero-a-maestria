a0f0a787f1ed5b949ce9ee6ba829ae5cc76e82fd
Caique Martins <caiquemartins.dev@gmail.com>
2022-10-28 09:14:45 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// Components
+import PrimeiroComponente from './components/primeiroComponente';
+import TemplateExpressions from './components/TemplateExpressions';
+import Challenge from './components/Challenge';
+import UserDetails from './components/UserDetails';
+//styles CSS
 import './App.css';
+
+const pessoas = [
+  {id: 0, nome: "Caíque", idade: 28, profissao: "estudante"},
+  {id: 1, nome: "Anderson", idade: 58, profissao: "carteiro"},
+  {id: 2, nome: "Iane", idade: 16, profissao: "baba"},
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Olá React</h1>
+      <p>Meu primeiro App</p>
+      <PrimeiroComponente/>
+      <TemplateExpressions/>
+      <Challenge/>
+      {pessoas.map((pessoas) =>(
+      <UserDetails 
+      key={pessoas.id}
+      nome={pessoas.nome} 
+      idade={pessoas.idade} 
+      profissao={pessoas.profissao}   />)
+      )}
     </div>
   );
 }
